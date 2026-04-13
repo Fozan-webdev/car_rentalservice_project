baa44c5fadf17d1fd706d44a2adaeb043fa691c2
unknown <asadullahnoor181eb@gmail.com>
2026-04-13 11:36:01 +0500

    AddCar-continue

--- Car_Rental_Services/admin/src/App.jsx
+++ Car_Rental_Services/admin/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import Navbar from './components/Navebar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import AddCar from "./components/AddCar";
+
+
 const App = () => {
   return (
-    <Navbar />
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<AddCar />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
