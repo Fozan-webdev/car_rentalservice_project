e4cd55226071d6d8649205e5a6f4adb36fc1922e
Muhammad Fozan <fozang105@gmail.com>
2026-04-15 15:03:16 +0500

    Booking

--- Car_Rental_Services/admin/src/App.jsx
+++ Car_Rental_Services/admin/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import AddCar from "./components/AddCar";
 import ManageCar from "./components/ManageCar";
+import Booking from "./components/Booking";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<AddCar />} />
         <Route path="/manage-cars" element={<ManageCar />} />
+        <Route path="/bookings" element={<Booking />} />
       </Routes>
     </>
   );
